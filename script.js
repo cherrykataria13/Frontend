33b2676694e48cc01e66f6fc5f843e7e366ba667
Cherry Kataria <cherrykamalkataria13@gmail.com>
2023-08-28 00:06:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const chooseFileButton = document.getElementById('chooseFileButton');
+const fileInput = document.getElementById('fileInput');
+const selectedFileName = document.getElementById('selectedFileName');
+const uploadButtonContainer = document.getElementById('uploadButtonContainer');
+const fileInfoContainer = document.getElementById('fileInfoContainer');
+const infoContainer = document.getElementById('infoContainer');
+const originalName = document.getElementById('originalName');
+const fileSize = document.getElementById('fileSize');
+const mimeType = document.getElementById('mimeType');
+
+chooseFileButton.addEventListener('click', () => {
+  fileInput.click();
+});
+
+fileInput.addEventListener('change', () => {
+  if (fileInput.files.length > 0) {
+    selectedFileName.textContent = fileInput.files[0].name;
+    fileInfoContainer.style.display = 'block';
+    uploadButtonContainer.style.display = 'block';
+  }
+});
+
+document.getElementById('uploadForm').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(document.getElementById('uploadForm'));
+  const response = await fetch('https://rf-backend-production.up.railway.app/upload', {
+    method: 'POST',
+    body: formData
+  });
+
+  const data = await response.json();
+
+  originalName.textContent = data.fileInfo.originalName;
+  fileSize.textContent = data.fileInfo.size;
+  mimeType.textContent = data.fileInfo.mimeType;
+  infoContainer.style.display = 'block';
+});
+
